028795319d1fa1f1eceae8015bb230437ebafeec
Lenochka <lvutina.en@gmail.com>
2022-06-21 23:13:28 +0300

    feat: fix mouse move

--- index.js
+++ index.js
@@ -15,32 +15,32 @@ const wss = new WebSocketServer({
 
 wss.on('connection', ws=>{
     console.log('connection accepted')
-    ws.send('something');
+    ws.send('connection settled');
 
     ws.on('message',async wsData => {
-
+        let mousePos = robot.getMousePos();
         const data = wsData.toString().split(' ');
         let moveDirection = data[0];
         let movePixels = data[1];
         switch (moveDirection){
             case 'mouse_up':
                 console.log ('mouse up');
-                robot.moveMouse(0, -Number(movePixels));
+                robot.moveMouse(mousePos.x, mousePos.y-Number(movePixels));
                 break;
             case 'mouse_down':
                 console.log ('mouse down');
-                robot.moveMouse(0, Number(movePixels));
+                robot.moveMouse(mousePos.x,mousePos.y+ Number(movePixels));
                 break;
             case 'mouse_left':
                 console.log ('mouse left');
-                robot.moveMouse(-Number(movePixels), 0);
+                robot.moveMouse(mousePos.x-Number(movePixels), mousePos.y);
                 break;
             case 'mouse_right':
                 console.log ('mouse right');
-                robot.moveMouse(Number(movePixels), 0);
+                robot.moveMouse(mousePos.x + Number(movePixels), mousePos.y);
                 break;
             default:
-                console.log ('default');
+                console.log ('error occurred');
         }
     });
 
